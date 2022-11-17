c2b46a85542612075b86388e4a88d3db43e49ea6
Jonas B. Franco <jonasbfranco@hotmail.com>
2022-11-17 16:25:07 -0300

    :tada: img logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,13 @@
 import styles from './NavBar.module.css'
+import Image from 'next/image'
+
 
 export default function NavBar() {
     return (
         <div className={styles.navbar_main}>
+            <div >
+                <Image src="/jonas.png" alt="Jonas B. Franco" width={150} height={150} className={styles.img_logo} />
+            </div>
             <ul>
                 <li>Github</li>
                 <li>Linkedin</li>
